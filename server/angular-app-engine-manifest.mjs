9f22689f1646224260286eb106cc55245d4c4d9d
samba <samba@gmail.com>
2026-02-23 02:27:12 +0100

    Auto-generated commit

---
+++ server/angular-app-engine-manifest.mjs
@@ -0,0 +1,10 @@
+
+export default {
+  basePath: 'https://Samba-SISSOKO.github.io/carambar-front',
+  supportedLocales: {
+  "en-US": ""
+},
+  entryPoints: {
+    '': () => import('./main.server.mjs')
+  },
+};
